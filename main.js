9386fab5fd8cc80930d514ad7f63a3eb08694967
wen <hlwen4391@gmail.com>
2025-12-01 18:33:43 +0800

    2025.12.01

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ class Character {
         this.defense = defense;
     }//定义角色属性
 
-
+    //攻击目标方法 
     attackTarget(target) {
         const damage = Math.max(this.attack - target.defense, 1);//攻击-防御=伤害
         target.hp -= damage;//血量-伤害=剩余血量
@@ -22,17 +22,17 @@ class Character {
 
 }
 
-const hero = new Character("英雄", 100, 10, 5); //hero的实例化
 
+//怪物类
 class Monster extends Character {
     constructor(name, hp, attack, defense) {
-        super(name, hp,
-            attack, defense);
-        this.miss = 0.99; //怪物攻击有30%的概率miss
+        super(name, hp, attack, defense);//调用父类构造函数
+        this.miss = 0.45;//怪物攻击有45%的概率miss
     }
     attackTarget(target) {
-        const random = Math.random();
+        const random = Math.random();//生成0-1之间的随机数
         console.log(`${this.name}攻击${target.name}的概率为${random}`);
+        //如果随机数小于miss，则攻击失败
         if (random < this.miss) {
             console.log(`${this.name}攻击${target.name}失败！`);
             return;
@@ -41,7 +41,31 @@ class Monster extends Character {
     }
 }
 
+
+
+//英雄类
+class Hero extends Character {
+    constructor(name, hp, attack, defense) {
+        super(name, hp, attack, defense);//调用父类构造函数
+        this.miss = 0.2;//英雄攻击有20%的概率miss
+    }
+    attackTarget(target) {
+        const radom = Math.random();//生成0-1之间的随机数
+        console.log(`${this.name}攻击${target.name}的概率是${radom}}`);
+        //如果随机数小于miss，则攻击失败
+        if (radom < this.miss) {
+            console.log(`${this.name}攻击${target.name}失败！`);
+            return;
+        }
+        super.attackTarget(target);
+    }
+
+
+}
+
+const hero = new Hero("英雄", 100, 10, 5); //hero的实例化
 const monster = new Monster("怪物", 150, 20, 8); //monster的实例化
+
 
 
 console.log("战斗开始！")
